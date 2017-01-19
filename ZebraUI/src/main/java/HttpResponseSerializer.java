569db5439fb9a309ebd6e522a193a05f9e5f7d0b
carlericsson <carl.ericsson@apicasystem.com>
2017-01-19 10:53:59 +0100

    Working Response Content

--- ZebraUI/src/main/java/HttpResponseSerializer.java
+++ ZebraUI/src/main/java/HttpResponseSerializer.java
@@ -2,6 +2,7 @@ import com.google.gson.*;
 import dfischer.proxysniffer.HttpResponse;
 
 import java.lang.reflect.Type;
+import java.util.Base64;
 
 /**
  * Created by carlericsson on 16/01/17.
@@ -11,14 +12,31 @@ public class HttpResponseSerializer implements JsonSerializer<HttpResponse> {
     @Override
     public JsonElement serialize(final HttpResponse httpResponse, final Type type, final JsonSerializationContext context) {
         final JsonObject jsonObj = new JsonObject();
-        jsonObj.add("content", context.serialize(httpResponse.getContent()));
+        jsonObj.add("content", context.serialize(createContent(httpResponse)));
         jsonObj.add("header", context.serialize(httpResponse.getHeader()));
         jsonObj.add("headerContentType", context.serialize(httpResponse.getContentType()));
         jsonObj.add("headerServerStatus", context.serialize(httpResponse.getStatus()));
         jsonObj.add("headerServerStatusText", context.serialize(httpResponse.getStatusText()));
         jsonObj.add("headerContentLength", context.serialize(httpResponse.getContentLength()));
+
+        System.out.println(createContent(httpResponse));
         return jsonObj;
 
+    }
+
+    private String createContent(HttpResponse response){
+        if (response.hasContent()){
+            if (response.getContentType()!=null && response.getContentType().contains("IMAGE")){
+                return new String(Base64.getEncoder().encode(response.getContent()));
+            }else {
+                String content = new String(response.getContent());
+                String content2 = content.replace("<","&lt;");
+                String content3 = content2.replace(">","&gt;");
+                return new String(content3);
+            }
+        }else {
+            return "null";
+        }
     }
 
 }
